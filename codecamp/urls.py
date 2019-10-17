a7e60ea4fac6235f833f29e604586e4521d18853
andrew <andybissel@protonmail.com>
2019-10-17 13:25:31 -0400

    django and REST-API baseline implemented

--- codecamp/urls.py
+++ codecamp/urls.py
@@ -12,10 +12,14 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
+TODO:
+    - "/" path doesn't work yet
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('codecamp.apps.attendance.urls'))
 ]
